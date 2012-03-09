06486a9f786f214ad3fd7cd5bc52aa6402c88618
rashmi.nair <rashmi.nair@icicletech.com>
2012-03-09 13:53:07 +0530

    second commit

---
+++ rashapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rashapp::Application.config.secret_token = 'a37635bec18aaa7992534569e9df024cb471eaac642b1167a48a9d2b447fe2867eecbb9292a1da1d766c101a28db6a2e93fe090ae53c98f614b5b789a1e20d02'
